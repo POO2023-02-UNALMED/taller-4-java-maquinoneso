5deb9609235511ed389454b619c2f3bf7b579848
maquinoneso <141966669+maquinoneso@users.noreply.github.com>
2023-09-05 00:14:27 -0500

    taller 4 java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,12 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    public long cedula = 1;
     String nombre;
     static int totalPersonas;
-    
+
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
     public Persona(long cedula, String nombre) {
@@ -16,7 +15,7 @@ public class Persona {
         this.nombre = nombre;
         totalPersonas++;
     }
-    
+
     public Persona(String nombre, long cedula) {
         this.cedula = cedula;
         this.nombre = nombre;
@@ -33,7 +32,12 @@ public class Persona {
         this.nombre = "";
         totalPersonas++;
     }
-    
+
+    public Persona() {
+        this.cedula = 0;
+        totalPersonas++;
+    }
+
     public long getCedula() {
         return cedula;
     }
--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,11 +1,12 @@
 package classroom;
 
 public enum Tipo {
-	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
-	
-	private int codigo;
-	private String nombre;
-	public Tipo(int codigo,String nombre) {
+	DISCIPLINAR(10, "10"), FUNDAMENTACION(20, "Fund"), ELECTIVA(30, "ele");
+
+	public int codigo;
+	public String nombre;
+
+	private Tipo(int codigo, String nombre) {
 		this.codigo = codigo;
 		this.nombre = nombre;
 	}
